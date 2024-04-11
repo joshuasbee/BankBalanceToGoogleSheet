528a6c5a562b0553b9d9d692d6de2050e701b36e
bananablizzard <55606174+joshuasbee@users.noreply.github.com>
2024-04-11 09:46:03 -0400

    Fix gitignore

---
+++ sheets.py
@@ -0,0 +1,61 @@
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+def update_values(spreadsheet_id, range_name, value_input_option, _values):
+    """
+    Updates the value of a cell in a Google Sheet.
+    """
+    try:
+        credentials_file = 'credentials.json'  # Specify the path to your service account key file
+        creds = Credentials.from_service_account_file(
+            credentials_file,
+            scopes=['https://www.googleapis.com/auth/spreadsheets']
+        )
+        service = build("sheets", "v4", credentials=creds)
+        body = {"values": _values}
+        result = (
+            service.spreadsheets()
+            .values()
+            .update(
+                spreadsheetId=spreadsheet_id,
+                range=range_name,
+                valueInputOption=value_input_option,
+                body=body,
+            )
+            .execute()
+        )
+        print(f"{result.get('updatedCells')} cells updated.")
+        return result
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return error
+
+def update_google_sheet(value_to_update, cell: str):
+    # Specify the spreadsheet ID and range
+    spreadsheet_id = "1veu8ltplzC5vc_51Ug51twVumMSwEtbsiVTYyeaVfN4"
+    range_name = f"Accounts!{cell}"
+
+    # Update the value in the spreadsheet
+    update_values(
+        spreadsheet_id,
+        range_name,
+        "USER_ENTERED",
+        [[value_to_update]],
+    )
+
+if __name__ == "__main__":
+    # Specify the spreadsheet ID and range
+    spreadsheet_id = "1veu8ltplzC5vc_51Ug51twVumMSwEtbsiVTYyeaVfN4"
+    range_name = "Accounts!A2"
+
+    # Specify the value to be updated
+    value_to_update = [['a']]
+
+    # Update the value in the spreadsheet
+    update_values(
+        spreadsheet_id,
+        range_name,
+        "USER_ENTERED",
+        value_to_update,
+    )
---
+++ test_BECU.py
@@ -0,0 +1,48 @@
+from playwright.sync_api import sync_playwright
+import data
+from sheets import update_google_sheet
+#make this a class...
+def get_balance():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)  # Set headless to False to see the browser
+        page = browser.new_page()
+ 
+        # Replace 'your_website_url' with the URL of the website you want to login to
+        page.goto('https://onlinebanking.becu.org/BECUBankingWeb/Login.aspx')
+
+        # Replace 'username' and 'password' with your actual credentials
+        username = data.BECU_USERNAME
+        password = data.BECU_PASS
+
+        # Replace 'username_selector' and 'password_selector' with the CSS selectors of the username and password fields
+        username_selector = 'input[name="ctlSignon$txtUserID"]'
+        password_selector = 'input[name="ctlSignon$txtPassword"]'
+
+        page.fill(username_selector, username)
+        page.fill(password_selector, password)
+
+        # Replace 'login_button_selector' with the CSS selector of the login button
+        login_button_selector = 'input[name="ctlSignon$btnLogin"]'
+        page.click(login_button_selector)
+
+        page.wait_for_selector('table.dataTableXtended')
+
+        # Find the element containing the desired value
+        checking_element = page.query_selector('table.dataTableXtended tbody tr.footer td:nth-child(3)')        
+        # Extract the text content of the element
+        if checking_element:
+            checking = checking_element.inner_text()
+            print(f'{checking=}')
+        else:
+            print("Checking not found")
+            checking = -999
+
+
+        # Close the browser
+        browser.close()
+        return checking
+
+# Call the function to execute the login
+checking = get_balance()
+
+# update_google_sheet(checking, 'A2')
