7f79d84e998f3c33b3564c9513b493e161880c7f
bananablizzard <55606174+joshuasbee@users.noreply.github.com>
2024-04-13 11:20:19 -0400

    Editing

--- driver.py
+++ driver.py
@@ -1,20 +1,13 @@
 from test_BECU import BECU
-from test_CHASE import CHASE
 import sheets
 # Use threads to update all bank accounts simultaneously
 threads = []
 #BECU 
-# becu = BECU()
-# becu.start()
-# threads.append(becu)
-#CHASE
-chase = CHASE()
-chase.start()
-threads.append(chase)
+becu = BECU()
+becu.start()
+threads.append(becu)
 #SUNCOAST
-
 
 for t in threads: t.join()
 #SHEETS
-# sheets.update_google_sheet(becu.result, 'A2')
-print(f'{chase.result=}')
+sheets.update_google_sheet(becu.result, 'A2')
